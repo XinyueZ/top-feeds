87512617305048c0cb9a145e17bf474f684f96ae
XinyueZhao <zhao@eosnewmedia.de>
2015-04-25 22:52:52 +0200

    Bugfix for %

--- src/app/csdn/news-list.go
+++ src/app/csdn/news-list.go
@@ -61,7 +61,9 @@ func (self *NewsList) Create(cxt appengine.Context, chJsonStr chan *string) {
 					s := "[" //Start making a json result.
 					for _, v := range pNewsList.Channel.NewsEntries {
 						v.Title = strings.Replace(v.Title, "\"", "'", -1)
+						v.Title = strings.Replace(v.Title, "%", "％", -1)
 						v.Description = strings.Replace(v.Description, "\"", "'", -1)
+						v.Description = strings.Replace(v.Description, "%", "％", -1)
 						v.UrlMobile = strings.Replace(v.Url, "www", "m", -1)
 
 						loc, _ := time.LoadLocation("Asia/Shanghai")
